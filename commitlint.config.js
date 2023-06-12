df82f55d9b8345f82c8bc5de87ad6bc648a7094e
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-06-12 15:05:31 -0600

    build: update `commitlint` configuration file

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,23 @@
  * @file Manages the commitlint module configuration.
  */
 
-// ━━	EXPORT MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-module.exports = {
+// ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * User configuration for commitlint.
+ *
+ * @private
+ * @typedef {import('@commitlint/types').UserConfig} UserConfig
+ */
+
+// ━━ MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Configuration object for commitlint.
+ *
+ * @type {UserConfig}
+ */
+const commitlintConfiguration = {
   extends: ['@commitlint/config-conventional'],
 };
+
+// ━━	EXPORT MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+module.exports = commitlintConfiguration;
