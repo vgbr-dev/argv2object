a305f3432ef0ecc5177a07193f2d5e6b313570cf
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2025-05-06 14:54:58 -0600

    build: update `commitlint` configuration file

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -18,7 +18,17 @@
  */
 const commitlintConfiguration = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
+    ],
+    'subject-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+  },
 };
 
 // ━━	EXPORT MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-module.exports = commitlintConfiguration;
+export default commitlintConfiguration;
