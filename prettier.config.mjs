983f6b21d96d1250f7738b310196e99612ae9e82
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2025-11-29 16:01:17 -0600

    chore: update prettier configuration file

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,44 @@
-{
-  "overrides": [{
-    "files": [".prettierrc", ".babelrc", ".eslintrc", ".stylelintrc"],
-    "options": {
-      "parser": "json"
-    }
-  }],
-  "singleQuote": true,
-  "printWidth": 100,
-  "trailingComma": "all",
-  "arrowParens": "avoid"
-}
+/**
+ * @file Prettier Configuration for Code Formatting.
+ *
+ * @description Custom settings for Prettier, the code formatter. Define
+ * formatting rules to maintain consistency in your project's code, including
+ * quoting, semicolons, indentation, and line length.
+ *
+ * @see {@link https://prettier.io/docs/configuration | Prettier Configuration File}
+ * @module PrettierConfiguration
+ */
+
+// ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Type for Prettier configuration.
+ *
+ * @typedef {import("prettier").Config} PrettierConfigurationOptions
+ */
+
+// ━━ MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Prettier settings for the project.
+ *
+ * @constant prettierConfiguration
+ * @type {PrettierConfigurationOptions}
+ */
+const prettierConfiguration = {
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: ['.prettierrc', '.babelrc', '.eslintrc', '.stylelintrc'],
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default prettierConfiguration;
