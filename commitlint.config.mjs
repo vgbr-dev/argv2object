2cc6c9b1338be5e95db37e276a167f5038f709d9
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2025-11-29 16:08:27 -0600

    chore: update commitlint configuration file

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -27,6 +27,16 @@ const commitlintConfiguration = {
     'subject-max-length': [2, 'always', 72],
     'body-max-line-length': [2, 'always', 100],
     'footer-max-line-length': [2, 'always', 100],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
   },
 };
 
